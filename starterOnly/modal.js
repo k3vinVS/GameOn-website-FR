a4e2147c841ddb50f91d0d4a0da97f6d77f88880
KEVIN VAN SCHELLEBECK <kevin.vanschellebeck@gmail.com>
2022-03-19 16:56:08 +0100

    add_form_DOM-elements

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,15 @@ const modalbg = document.querySelector('.bground');
 const closeBtn = document.querySelector('.close');
 const modalBtn = document.querySelectorAll('.modal-btn');
 const formData = document.querySelectorAll('.formData');
+const form = document.querySelector('form');
+const inputFirstName = document.querySelector('#first');
+const inputLastName = document.querySelector('#last');
+const inputEmail = document.querySelector('#email');
+const inputBirthday = document.querySelector('#birthdate');
+const inputQuantity = document.querySelector('#quantity');
+const inputLocation = document.querySelectorAll('input[name="location"]');
+const inputCheckbox1 = document.querySelector('#checkbox1');
+const inputCheckbox2 = document.querySelector('#checkbox2');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
@@ -21,6 +30,9 @@ function launchModal() {
 	modalbg.style.display = 'block';
 }
 
+// close modal form
 closeBtn.addEventListener('click', () => {
 	modalbg.style.display = 'none';
 });
+
+// Modal Form
