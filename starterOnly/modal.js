59398bf529a2e6ad78b088ed75bbc1a86bc6fe36
KEVIN VAN SCHELLEBECK <kevin.vanschellebeck@gmail.com>
2022-03-20 18:20:02 +0100

    Modification_html + Modal_Email_Form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -9,6 +9,7 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector('.bground');
+const modalBody = document.querySelector('.modal-body');
 const closeBtn = document.querySelector('.close');
 const modalBtn = document.querySelectorAll('.modal-btn');
 const formData = document.querySelectorAll('.formData');
@@ -22,17 +23,43 @@ const inputLocation = document.querySelectorAll('input[name="location"]');
 const inputCheckbox1 = document.querySelector('#checkbox1');
 const inputCheckbox2 = document.querySelector('#checkbox2');
 
-// launch modal event
+// functions Modal Form ----------------------
+
+function validInput(input) {
+	input.style.border = '2px solid green';
+}
+function invalidInput(input) {
+	input.style.border = '2px solid red';
+}
+
+// launch modal event ------------------------
 modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
 
-// launch modal form
+// launch modal form -------------------------
 function launchModal() {
 	modalbg.style.display = 'block';
 }
 
-// close modal form
+// close modal form --------------------------
 closeBtn.addEventListener('click', () => {
 	modalbg.style.display = 'none';
 });
 
-// Modal Form
+// Modal Form -------------------------------
+
+// Modal Email Form -------------------------------
+
+form.email.addEventListener('change', function() {
+	validEmail(this);
+});
+
+const validEmail = function(acceptEmail) {
+	let emailRegExp = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g');
+
+	if (emailRegExp.test(acceptEmail.value)) {
+		validInput(inputEmail);
+	} else {
+		alert('Adresse non valide');
+		invalidInput(inputEmail);
+	}
+};
